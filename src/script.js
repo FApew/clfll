c0a289645784ace3163a3fa20e10a1a10605d69e
fffppp-pixel <francype06@gmail.com>
2024-03-26 19:48:59 +0100

    peru

---
+++ src/script.js
@@ -0,0 +1,11 @@
+
+const schoolButton = document.getElementById("schoolButton")
+schoolButton.addEventListener("click", () => {
+    window.open("https://www.calabreselevi.edu.it/")
+})
+
+const teamButton = document.getElementById("teamButton")
+teamButton.addEventListener("click", () => {
+    window.open("./assets/html/Squadra")
+})
+
